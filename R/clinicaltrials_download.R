ad121764877a9d7d18e9ff5cd3cc09a8d0afe4c3
Michael Sachs <sachsmc@gmail.com>
2014-09-26 10:03:31 -0400

    bug fixes

--- R/clinicaltrials_download.R
+++ R/clinicaltrials_download.R
@@ -1,14 +1,22 @@
 #' Downloads detailed information about clinical trials satisfying a query
 #'
-#' Given a query,
-#' downloads detailed study information from clinicaltrials.gov. Optionally
-#' includes results of completed trials.
+#' Given a query, or a dataframe containing trial identifiers, downloads
+#' detailed study information from clinicaltrials.gov. Optionally includes
+#' results of completed trials. The URL search is limited to 2000 characters,
+#' and count restricted searches are done by NCT ID, which are 11 characters
+#' long. Therefore, the effective maximum count allowed is roughly 100. If count
+#' is greater than 100, then the first 100 trials will be returned, with a
+#' warning. To return all results, use a query string and set count to NULL.
 #'
 #' @param query Search pattern as a string; a vector of key-value pairs is
 #'   interpreted as an advanced search and is therefore combined with '&'
-#' @param count Limit the results to a specified integer. Set to NULL to include all results.
+#' @param tframe Data frame containing trial identifiers, as returned by
+#'   \link{clinicaltrials_search}
+#' @param count Limit the results to a specified integer. Set to NULL to include
+#'   all results.
 #' @param include_results Logical. Include results of completed trials
-#' @param include_textblocks Logical. Include lengthy text descriptions and eligibility criteria.
+#' @param include_textblocks Logical. Include lengthy text descriptions and
+#'   eligibility criteria.
 #'
 #' @export
 #'
@@ -20,25 +28,62 @@
 #'
 #'
 clinicaltrials_download <-
-  function(query = NULL, count = 20, include_results = FALSE, include_textblocks = FALSE)
+  function(query = NULL, tframe = NULL, count = 20, include_results = FALSE, include_textblocks = FALSE)
   {
 
-    query_url <- "http://clinicaltrials.gov/ct2/results?"
-
-    query <- paste_query(query)
-
-    # count by default is 20, change to a very large number if count = NULL
-
-    if(is.null(count)) count <- 1e6  # there are currently 174862 trials as of 18-Sept-2014
     if(!is.integer(as.integer(count))) stop("Count must be a number")
-
-    count_str <- paste0("&count=", as.integer(count))
-
     inc_res <- ifelse(include_results, "&resultsxml=true", "&studyxml=true")
 
-    final_url <- paste0(query_url, query, inc_res, count_str)
+    if(!is.null(query)){
 
-    search_result <- httr::GET(final_url)
+      if(is.null(count)) {  # return all results
+
+        query_url <- "http://clinicaltrials.gov/ct2/results?"
+        query <- paste_query(query)
+
+        search_result <- httr::GET(paste0(query_url, query, inc_res))
+
+      } else {
+
+        tframe <- clinicaltrials_search(query, count)
+        ## if count is too big, but less than the nrow(tframe) return first 100 results with a warning
+
+        if(nrow(tframe) > 100 & count > 100){
+
+          dex <- 1:100
+          warning("Count is too large (>100), only returning top 100 results. Use query and count = NULL to return all results")
+
+        } else {
+
+          dex <- 1:min(count, nrow(tframe))
+
+        }
+
+        query_url <- "http://clinicaltrials.gov/ct2/results?id="
+        final_url <- paste0(query_url, paste(tframe$nct_id[dex], collapse = "+OR+"), inc_res)
+        search_result <- httr::GET(final_url)
+
+      } } else if(!is.null(tframe)) {
+
+              ## if count is too big, but less than the nrow(tframe) return first 100 results with a warning
+
+              if(count > 100 & count > nrow(tframe)){
+
+                dex <- 1:100
+                warning("Count is too large (>100), only returning top 100 results. Use query and count = NULL to return all results")
+
+              } else {
+
+                dex <- 1:count
+
+              }
+
+              query_url <- "http://clinicaltrials.gov/ct2/results?id="
+              final_url <- paste0(query_url, paste(tframe$nct_id[dex], collapse = "+OR+"), inc_res)
+              search_result <- httr::GET(final_url)
+
+      } else stop("No search performed")
+
 
     ## download and unzip to a temporary directory
 
--- tests/testthat/test-download.R
+++ tests/testthat/test-download.R
@@ -5,15 +5,19 @@ test_that("download function returns correct format", {
 
    # download returns correct number of trials
 
-  expect_equal(clinicaltrials_count("lyme disease"), length(unique(clinicaltrials_download("lyme disease", count = 1e6)$study_info$nct_id)))
+  expect_equal(clinicaltrials_count("lyme disease"), length(unique(clinicaltrials_download(query = "lyme disease", count = 1e6)$study_info$nct_id)))
 
 
   nores <- clinicaltrials_download(query = 'heart disease AND stroke AND California', count = 5)
   res <- clinicaltrials_download(query = 'heart disease AND stroke AND California', count = 5, include_results = TRUE)
 
-  expect_equal(length(unique(nores$study_info$nct_id)), 5)  # fails
+  expect_equal(length(unique(nores$study_info$nct_id)), 5)
+
+  expect_warning(clinicaltrials_download(query = 'heart disease AND stroke AND California', count = 105))
 
   expect_equal(names(nores), c("study_info", "locations", "interventions", "outcomes", "textblocks"))
   expect_equal(names(res), c("study_information", "study_results"))
+
+  expect_error(clinicaltrials_download(query = NULL, tframe = NULL))
 
 })
