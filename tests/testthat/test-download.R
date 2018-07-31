de8d04d27e925e5867c5e8518f9cfd9e8901c950
titaniumtroop <titaniumtroop@gmail.com>
2018-07-31 16:13:40 -0500

    Updated test_download to set parallel=FALSE
    Testing the clinicaltrials_download function using testthat in parallel mode threw errors and is not advisable per Hadley Wickham (see http://r-pkgs.had.co.nz/tests.html#test-cran). I set parallel = FALSE, and the package check now works. Other changes are automated by roxygen.

--- tests/testthat/test-download.R
+++ tests/testthat/test-download.R
@@ -21,11 +21,11 @@ test_that("download function returns correct format", {
 
    # download returns correct number of trials
 
-  expect_equal(clinicaltrials_count("lyme disease"), length(unique(clinicaltrials_download(query = "lyme disease", count = NULL)$study_info$nct_id)))
+  expect_equal(clinicaltrials_count("lyme disease"), length(unique(clinicaltrials_download(query = "lyme disease", count = NULL, parallel = FALSE)$study_info$nct_id)))
 
 
-  nores <- clinicaltrials_download(query = 'heart disease AND stroke AND California', count = 5)
-  res <- clinicaltrials_download(query = 'heart disease AND stroke AND California', count = 5, include_results = TRUE)
+  nores <- clinicaltrials_download(query = 'heart disease AND stroke AND California', count = 5, parallel = FALSE)
+  res <- clinicaltrials_download(query = 'heart disease AND stroke AND California', count = 5, include_results = TRUE, parallel = FALSE)
 
   expect_equal(length(unique(nores$study_info$nct_id)), 5)
 
@@ -34,16 +34,16 @@ test_that("download function returns correct format", {
   expect_equal(names(nores), c("study_info", "locations", "arms", "interventions", "outcomes", "textblocks"))
   expect_equal(names(res), c("study_information", "study_results"))
 
-  expect_error(clinicaltrials_download(query = NULL, tframe = NULL))
+  expect_error(clinicaltrials_download(query = NULL, tframe = NULL, parallel = FALSE))
 
-  testcase <- clinicaltrials_download("NCT01419197", include_results = TRUE)
+  testcase <- clinicaltrials_download("NCT01419197", include_results = TRUE, parallel = FALSE)
   #test$study_results$outcome_data$analysis_list
 
   # download returns for tframe parameter
   tframe.count <- clinicaltrials_count(query = 'heart disease AND stroke AND California') # 304L
   tframe <- clinicaltrials_search(query = 'heart disease AND stroke AND California', count = NULL) # n = 304
   completed.tframe <- tframe[tframe$status.text %in% c("Withdrawn", "Completed", "Terminated"),] # n = 155
-  completed.download.tframe.subset <- clinicaltrials_download(tframe = completed.tframe, count = 25, include_results = TRUE) # study_info :'data.frame': 25 obs. of 77 variables
+  completed.download.tframe.subset <- clinicaltrials_download(tframe = completed.tframe, count = 25, include_results = TRUE, parallel = FALSE) # study_info :'data.frame': 25 obs. of 77 variables
   #completed.download.tframe.full <- clinicaltrials_download(tframe = completed.tframe, count = NULL, include_results = TRUE) # study_info :'data.frame': 155 obs. of 143 variables
 
 })
